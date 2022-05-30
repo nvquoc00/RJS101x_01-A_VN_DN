b1cd64825568265834fddabaaaef0d7425f78016
nvquoc00 <vanquoc25090@gmail.com>
2022-05-31 00:46:22 +0700

    Add function cho phép thay đổi số cột hiển thị trên trang (2 cột, 3 cột, 6 cột...)

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -6,21 +6,41 @@ import {
   CardBody,
   CardGroup,
   CardTitle,
+  Button,
 } from "reactstrap";
 import dateFormat from "dateformat";
+
+const classChange = {
+  col1: "col-12 col-md-12 col-lg-12 mt-1",
+  col2: "col-6 col-md-6 col-lg-6 mt-1",
+  col3: "col-4 col-md-4 col-lg-4 mt-1",
+  col4: "col-3 col-md-3 col-lg-3 mt-1",
+  col6: "col-2 col-md-2 col-lg-2 mt-1",
+};
+
+let { col1, col2, col3, col4, col6 } = classChange;
 
 class StaffList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       onSelectedStaff: null,
+      classDefault: "col-12 col-md-6 col-lg-4 mt-3",
     };
   }
+
   onStaffSelect(staff) {
     this.setState({
       onSelectedStaff: staff,
     });
   }
+
+  onColumnSelect(col) {
+    this.setState({
+      classDefault: col,
+    });
+  }
+
   renderStaff(staff) {
     if (staff != null) {
       return (
@@ -63,7 +83,7 @@ class StaffList extends Component {
   render() {
     const staffList = this.props.staffs.map((staff) => {
       return (
-        <div className="col-md-12 mt-1">
+        <div className={this.state.classDefault}>
           <Card key={staff.id} onClick={() => this.onStaffSelect(staff)}>
             <CardBody>
               <CardTitle>{staff.name}</CardTitle>
@@ -74,6 +94,55 @@ class StaffList extends Component {
     });
     return (
       <div className="container">
+        <div className="row m-3">
+          <div className="col-2 ml-2">
+            <Button
+              outline
+              onClick={() => this.onColumnSelect(col6)}
+              color="primary"
+            >
+              6 cột
+            </Button>
+          </div>
+          <div className="col-2 ml-2">
+            <Button
+              outline
+              onClick={() => this.onColumnSelect(col4)}
+              color="secondary"
+            >
+              4 cột
+            </Button>
+          </div>
+          <div className="col-2 ml-2">
+            <Button
+              outline
+              onClick={() => this.onColumnSelect(col3)}
+              color="success"
+            >
+              3 cột
+            </Button>
+          </div>
+
+          <div className="col-2 ml-2">
+            <Button
+              outline
+              onClick={() => this.onColumnSelect(col2)}
+              color="info"
+            >
+              2 cột
+            </Button>
+          </div>
+
+          <div className="col-2 ml-2">
+            <Button
+              outline
+              onClick={() => this.onColumnSelect(col1)}
+              color="warning"
+            >
+              1 cột
+            </Button>
+          </div>
+        </div>
         <div className="row">{staffList}</div>
         <div className="row mt-5">
           {this.renderStaff(this.state.onSelectedStaff)}
