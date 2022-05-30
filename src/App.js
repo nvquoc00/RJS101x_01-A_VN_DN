04fd8c883adb1893984de9cb97732c8da6093da7
nvquoc00 <vanquoc25090@gmail.com>
2022-05-30 23:34:57 +0700

    Create UI for StaffList component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
-import Menu from "./components/MenuComponent";
-import { DISHES } from "./shared/dishes";
+import { Navbar, NavbarBrand } from "reactstrap";
+import StaffList from "./components/StaffListComponent";
+import { STAFFS } from "./shared/staffs";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES,
+      staffs: STAFFS,
     };
   }
+
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
           </div>
         </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <StaffList staffs={this.state.staffs} />
       </div>
     );
   }
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,8 +1,64 @@
 import React, { Component } from "react";
+import { Card, CardImg, CardTitle, CardBody, CardText } from "reactstrap";
+import dateFormat from "dateformat";
 
 class StaffList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      onSelectedStaff: null,
+    };
+  }
+  onStaffSelect(staff) {
+    this.setState({
+      onSelectedStaff: staff,
+    });
+  }
+  renderStaff(staff) {
+    if (staff != null) {
+      return (
+        <div className="col-12">
+          <Card>
+            <CardImg width="100%" src={staff.image} alt={staff.name} />
+            <CardBody>
+              <CardTitle>Họ và tên: {staff.name}</CardTitle>
+              <CardText>
+                Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
+              </CardText>
+              <CardText>
+                Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
+              </CardText>
+              <CardText>Phòng ban: {staff.department.name}</CardText>
+              <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
+              <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
   render() {
-    return <div></div>;
+    const staffList = this.props.staffs.map((staff) => {
+      return (
+        <div className="col-md-12 mt-1">
+          <Card key={staff.id} onClick={() => this.onStaffSelect(staff)}>
+            <CardBody>
+              <CardTitle>{staff.name}</CardTitle>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    });
+    return (
+      <div className="container">
+        <div className="row">{staffList}</div>
+        <div className="row mt-5">
+          {this.renderStaff(this.state.onSelectedStaff)}
+        </div>
+      </div>
+    );
   }
 }
 
