1b010c48b47cfb748ce5714fdfaf2578791c1526
nvquoc00 <vanquoc25090@gmail.com>
2022-05-31 00:08:50 +0700

    update UI

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardTitle, CardBody, CardText } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardGroup,
+  CardTitle,
+} from "reactstrap";
 import dateFormat from "dateformat";
 
 class StaffList extends Component {
@@ -18,21 +25,35 @@ class StaffList extends Component {
     if (staff != null) {
       return (
         <div className="col-12">
-          <Card>
-            <CardImg width="100%" src={staff.image} alt={staff.name} />
-            <CardBody>
-              <CardTitle>Họ và tên: {staff.name}</CardTitle>
-              <CardText>
-                Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
-              </CardText>
-              <CardText>
-                Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
-              </CardText>
-              <CardText>Phòng ban: {staff.department.name}</CardText>
-              <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
-              <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
-            </CardBody>
-          </Card>
+          <CardGroup>
+            <div className="col-4">
+              <Card>
+                <CardImg
+                  width="100%"
+                  height="260px"
+                  src={staff.image}
+                  alt={staff.name}
+                />
+              </Card>
+            </div>
+            <div className="col-8">
+              <Card>
+                <CardBody>
+                  <CardTitle>Họ và tên: {staff.name}</CardTitle>
+                  <CardText>
+                    Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
+                  </CardText>
+                  <CardText>
+                    Ngày vào công ty:{" "}
+                    {dateFormat(staff.startDate, "dd/mm/yyyy")}
+                  </CardText>
+                  <CardText>Phòng ban: {staff.department.name}</CardText>
+                  <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
+                  <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
+                </CardBody>
+              </Card>
+            </div>
+          </CardGroup>
         </div>
       );
     } else {
