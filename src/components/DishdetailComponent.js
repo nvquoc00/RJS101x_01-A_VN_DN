e1727ebf030c8a1fdba1bff7cb26edc48b25f8bb
nvquoc00 <vanquoc25090@gmail.com>
2022-06-02 00:26:42 +0700

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
-class DishDeatail extends Component {
+class DishDetail extends Component {
   renderDish(dish) {
     return (
       <div className="col-12 col-md-5 m-1">
@@ -26,7 +26,12 @@ class DishDeatail extends Component {
                 <li key={comment.id}>
                   <p>{comment.comment}</p>
                   <p>
-                    -- {comment.author}, {comment.date}
+                    -- {comment.author},{" "}
+                    {new Intl.DateTimeFormat("en-US", {
+                      year: "numeric",
+                      month: "short",
+                      day: "2-digit",
+                    }).format(new Date(Date.parse(comment.date)))}
                   </p>
                 </li>
               );
@@ -55,4 +60,4 @@ class DishDeatail extends Component {
   }
 }
 
-export default DishDeatail;
+export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
-import DishDeatail from "./DishdetailComponent";
+import DishDetail from "./DishdetailComponent";
 
 class Menu extends Component {
   constructor(props) {
@@ -18,7 +18,7 @@ class Menu extends Component {
     const menu = this.props.dishes.map((dish) => {
       return (
         <div className="col-12 col-md-5 m-1">
-          <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+          <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
               <CardTitle>{dish.name}</CardTitle>
@@ -31,7 +31,7 @@ class Menu extends Component {
       <div className="container">
         <div className="row">{menu}</div>
         <div className="row">
-          <DishDeatail dish={this.state.selectedDish} />
+          <DishDetail dish={this.state.selectedDish} />
         </div>
       </div>
     );
